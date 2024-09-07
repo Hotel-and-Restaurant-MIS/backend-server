d18611f2f0b431d75fba7d867ea99cd60f750d64
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-07 10:14:38 +0530

    OrderItem class is created.

---
+++ restaurant-order-service/src/main/java/com/luxury/restaurant_order_service/model/OrderItem.java
@@ -0,0 +1,34 @@
+package com.luxury.restaurant_order_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long orderItemId;
+
+    @OneToOne
+    @JoinColumn(name = "menu_item_id",nullable = false)
+    private MenuItem menuItem;
+
+    private Integer quantity;
+    private Double totalPrice;
+    private String specialNote;
+    private Integer tableId;
+    private Date dateTime;
+
+    @OneToMany
+    @JoinColumn(name="selected_addon",nullable = false)
+    private List<AddOn> selectedAddOns;
+
+}
