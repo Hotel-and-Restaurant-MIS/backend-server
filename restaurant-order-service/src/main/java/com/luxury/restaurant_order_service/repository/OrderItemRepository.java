4e78d2566d2b460a5e37d00b2a32411b75e8dd7b
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-08 07:26:15 +0530

    OrderItemRepository.java is created.

---
+++ restaurant-order-service/src/main/java/com/luxury/restaurant_order_service/repository/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.luxury.restaurant_order_service.repository;
+
+import com.luxury.restaurant_order_service.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+
+}
+
