3220a685d2bd5991b8f8f5445d67c0dab2081972
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-08 07:24:09 +0530

    Order class is created.

---
+++ restaurant-order-service/src/main/java/com/luxury/restaurant_order_service/model/Order.java
@@ -0,0 +1,33 @@
+package com.luxury.restaurant_order_service.model;
+
+import com.luxury.restaurant_order_service.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name="`Order`")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    private Double orderTotal;
+
+    @Enumerated(EnumType.ORDINAL)
+    private OrderStatus orderStatus;
+
+    @OneToMany
+    private List<OrderItem> orderItems;
+
+    private Integer tableId;
+}
