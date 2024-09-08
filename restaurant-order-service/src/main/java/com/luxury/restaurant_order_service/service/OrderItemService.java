dc685b054cb563316357e7bd4cfef28808fe3cae
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-08 07:25:33 +0530

    OrderItemService.java is created.

---
+++ restaurant-order-service/src/main/java/com/luxury/restaurant_order_service/service/OrderItemService.java
@@ -0,0 +1,24 @@
+package com.luxury.restaurant_order_service.service;
+
+import com.luxury.restaurant_order_service.model.OrderItem;
+import com.luxury.restaurant_order_service.repository.OrderItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderItemService {
+    private final OrderItemRepository orderItemRepository;
+
+    @Autowired
+    public OrderItemService(OrderItemRepository orderItemRepository){
+        this.orderItemRepository = orderItemRepository;
+    }
+
+    public List<OrderItem> getAllOrderItems(){
+        return orderItemRepository.findAll();
+    }
+
+
+}
