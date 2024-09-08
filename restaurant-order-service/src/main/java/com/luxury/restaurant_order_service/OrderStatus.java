03ffe5dc528b6d13c9adc986c23bb4894c7d533a
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-08 07:25:55 +0530

    OrderStatus enum is created

---
+++ restaurant-order-service/src/main/java/com/luxury/restaurant_order_service/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.luxury.restaurant_order_service;
+
+public enum OrderStatus {
+    PENDING,
+    PREPARING,
+    COMPLETED
+}
---
+++ restaurant-order-service/src/main/java/com/luxury/restaurant_order_service/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.luxury.restaurant_order_service.enums;
+
+public enum OrderStatus {
+    PENDING,PREPARING,COMPLETED
+}
