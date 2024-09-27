2cf1affa4138bbfc5a0a7a1a597412096d3ffecd
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-09-28 00:08:53 +0530

    added url predicate vw to all controllers

--- virtual-waiter-service/src/main/java/com/luxury/virtualwaiter_service/controller/AddOnController.java
+++ virtual-waiter-service/src/main/java/com/luxury/virtualwaiter_service/controller/AddOnController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/addOn")
+@RequestMapping("/vw/addOn")
 public class AddOnController {
 
     private final AddOnService addOnService;
--- virtual-waiter-service/src/main/java/com/luxury/virtualwaiter_service/controller/SingleTableOrderController.java
+++ virtual-waiter-service/src/main/java/com/luxury/virtualwaiter_service/controller/SingleTableOrderController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/singleTableOrder")
+@RequestMapping("/vw/singleTableOrder")
 public class SingleTableOrderController {
 
     private final SingleTableOrderService singleTableOrderService;
