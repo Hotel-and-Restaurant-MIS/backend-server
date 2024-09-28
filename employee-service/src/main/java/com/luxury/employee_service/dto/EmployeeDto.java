085a586e789216a477f309a59c258a35d5159e52
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-28 17:55:40 +0530

    create EmployeeDto for return the new Employee object.

---
+++ employee-service/src/main/java/com/luxury/employee_service/dto/EmployeeDto.java
@@ -0,0 +1,13 @@
+package com.luxury.employee_service.dto;
+
+import lombok.Builder;
+
+@Builder
+public record EmployeeDto(
+        String name,
+        String nic,
+        String email,
+        String phoneNumber,
+        String role
+) {
+}
