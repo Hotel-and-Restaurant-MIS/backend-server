66db09f9b6a78e6d0767b06370c1b0b6fbc1a59f
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-09-28 17:57:13 +0530

    return EmployeeDto

--- employee-service/src/main/java/com/luxury/employee_service/service/EmployeeService.java
+++ employee-service/src/main/java/com/luxury/employee_service/service/EmployeeService.java
@@ -1,8 +1,10 @@
 package com.luxury.employee_service.service;
 
+import com.luxury.employee_service.dto.EmployeeDto;
 import com.luxury.employee_service.model.Employee;
 import com.luxury.employee_service.model.EmployeeRole;
 import com.luxury.employee_service.repository.EmployeeRepository;
+import com.luxury.employee_service.repository.EmployeeRoleRepository;
 import com.luxury.employee_service.rto.EmployeeRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,29 +15,45 @@ import java.util.List;
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final EmployeeRoleRepository employeeRoleRepository;
 
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository, EmployeeRoleRepository employeeRoleRepository) {
         this.employeeRepository = employeeRepository;
+        this.employeeRoleRepository = employeeRoleRepository;
     }
 
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
-    public Employee addEmployee(EmployeeRecord employeeRecord) {
+    public EmployeeDto addEmployee(EmployeeRecord employeeRecord) {
         try {
-            var newEmployee = Employee.builder()
-                    .name(employeeRecord.name())
-                    .nic(employeeRecord.nic())
-                    .phoneNumber(employeeRecord.phoneNumber())
-                    .email(employeeRecord.email())
-                    .employeeRole(
-                            EmployeeRole.builder()
-                                    .roleId(employeeRecord.roleId()). build()
-                    ).build();
+            var newEmployee = employeeRepository.save(
+                    Employee.builder()
+                            .name(employeeRecord.name())
+                            .nic(employeeRecord.nic())
+                            .phoneNumber(employeeRecord.phoneNumber())
+                            .email(employeeRecord.email())
+                            .employeeRole(
+                                    EmployeeRole.builder()
+                                            .roleId(employeeRecord.roleId()). build()
+                            ).build()
+            );
 
-            return employeeRepository.save(newEmployee);
+            var role = employeeRoleRepository.findById( newEmployee.getEmployeeRole().getRoleId().intValue()).orElse(null);
+
+            assert role != null;
+            return EmployeeDto.builder()
+                    .name(newEmployee.getName())
+                    .email(newEmployee.getEmail())
+                    .nic(newEmployee.getNic())
+                    .phoneNumber(newEmployee.getPhoneNumber())
+                    .role(role.getRoleType())
+                    .build();
+
+
+
         }catch(Exception e) {
             throw new RuntimeException("Failed to add reservation", e);
         }
