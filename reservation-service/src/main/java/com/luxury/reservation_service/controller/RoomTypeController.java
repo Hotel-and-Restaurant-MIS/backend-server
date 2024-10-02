0e266157ce752e354c6f1292850a4fde59faf0a0
Sunera Sandaruwan <sandaruwankds.21@uom.lk>
2024-10-02 22:58:20 +0530

    end point updated

--- reservation-service/src/main/java/com/luxury/reservation_service/controller/RoomTypeController.java
+++ reservation-service/src/main/java/com/luxury/reservation_service/controller/RoomTypeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reservations/roomtype")
+@RequestMapping("/roomtype")
 public class RoomTypeController {
 
     private final RoomTypeService roomTypeService;
