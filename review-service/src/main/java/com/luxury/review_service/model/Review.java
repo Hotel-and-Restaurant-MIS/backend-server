a86370913abd15c5e107fd9d5629fad69f1b3dff
Sunera Sandaruwan <sandaruwankds.21@uom.lk>
2024-10-08 12:51:25 +0530

    review id set to auto increment

--- review-service/src/main/java/com/luxury/review_service/model/Review.java
+++ review-service/src/main/java/com/luxury/review_service/model/Review.java
@@ -1,6 +1,8 @@
 package com.luxury.review_service.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -12,6 +14,7 @@ import lombok.*;
 @Builder
 public class Review {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
