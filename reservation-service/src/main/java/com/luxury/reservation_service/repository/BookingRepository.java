f8ec7dbc9ac253edb15bf93d90c87bae892320a4
Sunera25 <sandaruwankds.21@uom.lk>
2024-10-18 17:02:58 +0530

    test

--- reservation-service/src/main/java/com/luxury/reservation_service/repository/BookingRepository.java
+++ reservation-service/src/main/java/com/luxury/reservation_service/repository/BookingRepository.java
@@ -10,6 +10,5 @@ import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    List<Booking> findByRoomTypeName(RoomType roomTypeName);
     List<Booking> findByCheckoutDateBefore(LocalDate date);
 }
